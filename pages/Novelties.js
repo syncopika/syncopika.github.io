9a830c622de6639f29262491424749a8f6762904
nicholas hung <nhung93@outlook.com>
2021-06-11 23:31:14 -0400

    added a new novel application. and minor edits to the novelties page.

--- pages/Novelties.js
+++ pages/Novelties.js
@@ -1,11 +1,14 @@
 const Novelties = {
 	template: 
 		`<div id='novelties'>
-			<h2>fun little programs</h2>
+			<br />
+			<p> These applications are pretty small and more like idea prototypes so not a lot of time has been put into them (compared to some of my other work). </p>
+			<p> But hopefully they may serve as springboards for new ideas! </p>
 			<hr>
 			<p><a href='misc/karaokeget.html'> karaoke-get </a></p>
 			<p><a href='https://syncopika.github.io/chinese_quiz/'> chinese quiz (普通話) </a></p>
-			<p><a href='misc/fun-text-stuff.html'> fun text manipulation </a></p>
+			<p><a href='misc/memory-cards.html'> memory card game </a></p>
+			<p><a href='misc/fun-text-stuff.html'> text manipulation </a></p>
 			<p><a href='misc/slot-machine.html'> vtuber slot machine </a></p>
 			<p><a href='https://syncopika.github.io/visual_novel_maker/'> visual novel examples </a></p>
 		</div>`
