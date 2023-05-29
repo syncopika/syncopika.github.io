3c53a13fbe2d30265d9bf849f76a244da389c980
nicholas hung <nhung93@outlook.com>
2023-05-29 18:39:28 -0400

    update 'other software' page

--- pages/Novelties.js
+++ pages/Novelties.js
@@ -8,12 +8,12 @@ const Novelties = {
             <p><a href='https://github.com/syncopika/artstation'> artstation </a></p>
             <p><a href='https://github.com/syncopika/basic_screen_recorder_windows'> screen recorder </a></p>
             <br />
-            <h3> mobile </h3>
-            <p><a href='https://github.com/syncopika/mobile-qr-code-scanner'>QR code scanner for Android</a></p>
-            <p><a href='https://github.com/syncopika/digital-ink-recognition-android'>Chinese character recognition app for Android</a></p>
+            <h3> mobile (Android) </h3>
+            <p><a href='https://github.com/syncopika/mobile-qr-code-scanner'>QR code scanner</a></p>
+            <p><a href='https://github.com/syncopika/digital-ink-recognition-android'>Chinese character recognition app</a></p>
             <br />
             <h3> web </h3>
-            <p><a href='https://syncopika.github.io/chinese_quiz/'> Chinese quiz (普通話) </a></p>
+            <p><a href='https://syncopika.github.io/flashcards/'> flashcards </a></p>
             <p><a href='misc/karaokeget.html'> karaoke-get </a></p>
             <p><a href='misc/webm-to-wav.html'> webm-to-wav converter </a></p>
             <p><a href='misc/slot-machine.html'> vtuber slot machine </a></p>
